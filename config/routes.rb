f356936d287cd25579c3a2e9de030bd3d399d5d8
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-07-30 04:16:12 +0300

    commit tree 5000f5a6aa91a34b3be8e78baba75245ddba04fa

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :items, only: [:index,:show,:create] 
 
-  resources :users, only: [:index,:show] do 
+  resources :users, only: [:index,:show,:create] do 
     resources :items, only: [:index,:show,:create] 
   end 
 
